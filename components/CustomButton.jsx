6d3b74c489972df14dbbd6a0cd9958f2e4de77fc
PUNEET VAISHNAV <65516671+iamfutureflash@users.noreply.github.com>
2024-04-19 02:01:34 +0530

    initialized project , create major routes and project setup added all the assets abd 90% build home page

---
+++ components/CustomButton.jsx
@@ -0,0 +1,17 @@
+import { View, Text, TouchableOpacity } from 'react-native'
+import React from 'react'
+
+const CustomButton = ({ title, handlePress, containerStyles, textStyles, isLoading }) => {
+    return (
+        <TouchableOpacity
+            onPress={handlePress}
+            activeOpacity={0.7}
+            className={`bg-secondary min-h-[62px justify-center items-center rounded-xl ${containerStyles} ${isLoading ? 'opacity-50' : ''}`}
+            disabled={isLoading}
+        >
+            <Text className={`text-primary font-psemibold text-lg px-4 py-3`}>{title}</Text>
+        </TouchableOpacity>
+    )
+}
+
+export default CustomButton
